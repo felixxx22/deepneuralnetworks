a55708fc7a651175e24a0a934e422e13bd99236d
Felix <felix.chunggg@gmail.com>
2022-06-21 23:35:41 +1000

    modelling is very bad

--- ass4.py
+++ ass4.py
@@ -9,6 +9,8 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Activation
 from tensorflow.keras.callbacks import EarlyStopping
 from sklearn import metrics
+from scipy.stats import zscore
+import matplotlib.pyplot as plt
 
 # import data
 file='C:\\Users\\felix\\github\\deepneuralnetworks\\crx.csv'
@@ -26,51 +28,42 @@ df = df.drop(['a1', 'a4', 'a5', 'a6', 'a7','a16'], axis=1)
 # create dummy variables for dataframe
 df = pd.get_dummies(df)
 
-# 'shuffle' data (reindex)
-df = df.reindex(np.random.permutation(df.index))
+# standardise ranges
+df['s3'] = zscore(df['s3'])
+df['a8'] = zscore(df['a8'])
+df['a11'] = zscore(df['a11'])
+df['a15'] = zscore(df['a15'])
 
-# split data into testing, validation and training
-mask = np.random.rand(len(df))<0.8
-traindf = pd.DataFrame(df[mask])
-testdf = pd.DataFrame(df[~mask])
+# create to numpy - classification data
+x_columns = df.columns.drop('a2').drop('a14')
+x = df[x_columns].values
+y_a2 = df['a2'].values
+y_a14 = df['a14'].values
 
-mask = np.random.rand(len(traindf))<0.8
-validationdf = pd.DataFrame(traindf[~mask])
-traindf = pd.DataFrame(traindf[mask])
-
-
-print(f"Training df: {len(traindf)}")
-print(f"test df: {len(testdf)}")
-
-
-# create x table
-x_columns = traindf.columns.drop(['a2', 'a14'])
-train_x = traindf[x_columns] # need to get dummy columns manually
-test_x = testdf[x_columns]
-validation_x = validationdf[x_columns]
-
-# create a2 table
-train_y_a2 = traindf['a2']
-test_y_a2 = testdf['a2']
-validation_y_a2 = validationdf['a2']
-
-# create a14 table
-train_y_a14 = df['a14']
+# create train/test
+x_train, x_test, y_a2_train, y_a2_test = train_test_split(x, y_a2,test_size=0.10, random_state=1)
+x_train, x_val, y_a2_train, y_a2_val = train_test_split(x_train, y_a2_train,test_size=0.25, random_state=1)
 
 
 #####################################################
 # Build Model
 model = Sequential()
-model.add(Dense(10, input_dim=train_x.shape[1], activation='relu'))
+model.add(Dense(25, input_dim=x_train.shape[1], activation='relu'))
 model.add(Dense(10, activation='relu'))
 model.add(Dense(1))
 model.compile(loss='mean_squared_error', optimizer='adam')
 monitor = EarlyStopping(monitor='val_loss', min_delta=1e-3,patience=5,verbose=1, mode='auto', restore_best_weights=True)
-model.fit(train_x, train_y_a2, validation_data=(validation_x, validation_y_a2),callbacks=[monitor], verbose=1,epochs=1000)
+model.fit(x_train, y_a2_train, validation_data=(x_val, y_a2_val),callbacks=[monitor], verbose=2,epochs=1000)
 
-pred = model.predict(test_x)
-score = np.sqrt(metrics.mean_squared_error(pred,test_y_a2))
+pred = model.predict(x_test)
+score = np.sqrt(metrics.mean_squared_error(pred,y_a2_test))
 print(f"Final score (RMSE): {score}")
 
-print(len(pred))
-print(test_y_a2)
+# print regression chart
+t = pd.DataFrame({'pred': pred.flatten(), 'y': y_a2_test.flatten()})
+t.sort_values(by=['y'], inplace=True)
+plt.plot(t['y'].tolist(), label='expected')
+plt.plot(t['pred'].tolist(), label='prediction')
+plt.ylabel('output')
+plt.legend()
+plt.show()
